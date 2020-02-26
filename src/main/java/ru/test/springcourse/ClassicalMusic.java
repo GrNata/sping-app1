64de8c5ecebb5cf40947b66c3f0e284753fac277
GrNata <gr.natalij@gmail.com>
2020-02-26 20:56:13 +0300

    scope and init destroy

--- src/main/java/ru/test/springcourse/ClassicalMusic.java
+++ src/main/java/ru/test/springcourse/ClassicalMusic.java
@@ -1,6 +1,15 @@
 package ru.test.springcourse;
 
 public class ClassicalMusic implements Music {
+
+    public void doMyInit() {
+        System.out.println("Doing my initialization.");
+    }
+
+    public void doMyDestroy() {
+        System.out.println("Doing my destroy.");
+    }
+
     @Override
     public String getSong() {
         return "Bach Tokkata";
--- src/main/java/ru/test/springcourse/TestSpring.java
+++ src/main/java/ru/test/springcourse/TestSpring.java
@@ -10,23 +10,29 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 //        Music music = context.getBean("musicBean", Music.class);
 //        MusicPlayer musicPlayer = new MusicPlayer(music);
+
+        ClassicalMusic classicalMusic1 = context.getBean("musicBean", ClassicalMusic.class);
+        ClassicalMusic classicalMusic2 = context.getBean("musicBean", ClassicalMusic.class);
+        System.out.println(classicalMusic1.getSong());
+        System.out.println(classicalMusic2.getSong());
+
         // создаем зависимости вручную
-        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-
-        firstMusicPlayer.playMusic();
-        secondMusicPlayer.playMusic();
-
-        System.out.println(firstMusicPlayer.getName());
-        System.out.println(firstMusicPlayer.getVolume());
-
-        firstMusicPlayer.setVolume(100);
-
-        System.out.println(secondMusicPlayer.getName());
-        System.out.println(secondMusicPlayer.getVolume());
-
-        System.out.println(firstMusicPlayer.getName());
-        System.out.println(firstMusicPlayer.getVolume());
+//        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//
+//        firstMusicPlayer.playMusic();
+//        secondMusicPlayer.playMusic();
+//
+//        System.out.println(firstMusicPlayer.getName());
+//        System.out.println(firstMusicPlayer.getVolume());
+//
+//        firstMusicPlayer.setVolume(100);
+//
+//        System.out.println(secondMusicPlayer.getName());
+//        System.out.println(secondMusicPlayer.getVolume());
+//
+//        System.out.println(firstMusicPlayer.getName());
+//        System.out.println(firstMusicPlayer.getVolume());
 
         context.close();
 
